f754e038e2dfe4d4f68c8ee892819275a8c38f93
Meghan Joseph <majoseph77@aol.com>
2015-08-17 14:06:50 -0700

    start user and puppies controller

---
+++ controllers/Puppies.js
@@ -0,0 +1,21 @@
+var Puppy = require('./Puppy');
+
+var index = function(req, res, next){
+
+};
+
+var show = function(req, res, next){
+
+};
+
+var new = function(req, res, next){
+
+};
+
+var create = function(req, res, next){
+
+};
+
+var destroy = function(req, res, next){
+
+};
---
+++ controllers/Users.js
@@ -0,0 +1,31 @@
+var User = require('./User');
+
+var userIndex = function(req, res, next){
+
+};
+
+var show = function(req, res, next){
+
+};
+
+//new User
+var userNew = function(req, res, next){
+
+};
+//saves the new user to db this will go to one route
+var userCreate = function(req, res, next){
+
+};
+//new owner
+var ownerNew = function(req, res, next){
+
+};
+//saves the new owner to db this will go to a different route than n user
+var ownerCreate = function(req, res, next){
+
+};
+
+var destroy = function(req, res, next){
+
+};
+
