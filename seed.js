5f7e0aadcd551d0be04524de850c25d34535d551
kimo5532 <shotbyabel@gmail.com>
2015-08-17 14:48:15 -0700

    comment out kids and owner boolean for User model

---
+++ seed.js
@@ -0,0 +1,43 @@
+  var mongoose = require('mongoose');
+   mongoose.connect('mongodb://localhost:27017/pup_buds');
+
+  var   User    = require('./models/User'),
+  //       Owners  = require('./models/Owners'),
+  //       Puppies = require('/models/Puppies');
+
+
+  // var newOwners = [
+
+  // {
+
+  //  name:,
+  //  age:,
+  //  email:,
+  //  profile_pic:,
+  //  puppy: (embedded Puppies id)
+  //  zip:,
+
+  // },
+    // {
+
+  //  name:,
+  //  age:,
+  //  email:,
+  //  profile_pic:,
+  //  puppy: (embedded Puppies id)
+  //  zip:,
+
+  // },
+
+
+
+
+
+
+
+
+  // ];      
+
+
+
+
