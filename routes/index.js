67f0ae4595b921ec22273cad53bf5cb2c0a3ebdd
Meghan Joseph <majoseph77@aol.com>
2015-08-18 16:07:24 -0700

    changed naming conventions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
+var express           = require('express');
+var router            = express.Router();
+var passport          = require('passport');
 var PuppiesController = require('../controllers/Puppies');
 var SessionsController= require('../controllers/Sessions');
-var UsersController = require('../controllers/Users');
+var UsersController   = require('../controllers/Users');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Pup Buds' });
