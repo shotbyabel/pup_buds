547014c7129bb3fcdbbfe5912a7e72490a12ff66
kimo5532 <shotbyabel@gmail.com>
2015-08-20 15:22:39 -0700

    before I delete branches

--- io.js
+++ io.js
@@ -1,28 +1,23 @@
 var io = require('socket.io')();
 
-var chatNamespace = [];
+io.on('connection', function(socket) {
+  console.log('User connected');
 
-io.on('connection', function (socket) {
-  socket.on('register-chat', function(data){
-    // if the namespace already exists - set to false initally
-    var found = false;
-    for (var i = 0; i < chatNamespace.length; i++) {
-      if (chatNamespace[i].namespace === data.namespace) {
-        found = true;
-        break;
-      }
-      if (!found) {
-        var ns = {
-          namespace: data.namespace,
-          socket: io.of(data.namespace)
-        };
-        chatNamespace.push(ns);
-        ns.socket.on('send-msg', function(data){
-          ns.socket.emit('receive-msg', data);
-        });
-      }
-    }
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
   });
+
+  socket.on('register-chat', function(room){
+    console.log('message: ' + room);
+    socket.join(room);
+    console.log(socket);
+    socket.room = room;
+  });
+
+  socket.on('private-message', function(message) {
+    io.to(socket.room).emit('private-message', message);
+  });
+
 });
 
 
