53c4c9cb062a7f1777c38ee53fb387ef21252ce0
Ezra Raez <ezRAez10@gmail.com>
2015-08-19 22:35:32 -0700

    added logged_in authority to routes, added comments to sessions and routes

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,13 +3,15 @@ var passport = require('passport');
 var User = require('../models/User');
 var router = express.Router();
 
-//GET '/login'
+//||||||||||||||||||||||||||--
+// GET '/LOGIN'
+//||||||||||||||||||||||||||--
 function sessionsNew  (req, res) {
   res.render('auth/login', {user : req.user});
 };
-
-//Post actually logs in
-//double check this withOUt sessions Create..
+//||||||||||||||||||||||||||--
+// POST ACTUALLY LOGS IN DOUBLE CHECK THIS WITHOUT SESSIONS CREATE..
+//||||||||||||||||||||||||||--
 function sessionsCreate (req, res, next) {
     req.session.save(function (err) {
       if (err) return next(err);
@@ -17,8 +19,9 @@ function sessionsCreate (req, res, next) {
     });
   };
 
-
-//GET '/logout'
+//||||||||||||||||||||||||||--
+// GET '/LOGOUT'
+//||||||||||||||||||||||||||--
 function sessionsDelete  (req, res) {
   // req.logout();
   // res.redirect('/');
@@ -27,27 +30,11 @@ function sessionsDelete  (req, res) {
   })
 };
 
-// //GET /'secret'
-// function sessionsShow (req, res) {isLoggedIn,
-//   res.render('secret', {user: req.user});
-// };
-
-// middleware to make sure a user is logged in
-// function isLoggedIn (req, res, next) {
-//   // if user is authenticated in the session, carry on
-//   if (req.isAuthenticated())
-//     return next();
-//   // if they aren't redirect them to the login page
-//   res.redirect('/login');
-// }
-
-// router.get('/login', function
-//session create
-// router.get('/logout', function
-
+//||||||||||||||||||||||||||--
+// EXPORT FUNCTIONS
+//||||||||||||||||||||||||||--
 module.exports = {
   sessionsNew: sessionsNew,
   sessionsCreate: sessionsCreate,
-  // sessionsShow:   sessionsShow,
   sessionsDelete:  sessionsDelete
 };
