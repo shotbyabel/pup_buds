3cf6659f0971896bbf22798633432e89a81198aa
Sam Claney <sampclaney@gmail.com>
2015-08-20 13:38:38 -0700

    method override

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
